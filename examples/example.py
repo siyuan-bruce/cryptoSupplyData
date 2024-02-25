94755bec25af966b1a8fc2fe63cf2a72d6e6b3ec
BruceJin <siyuan.jin@connect.ust.hk>
2024-02-25 09:55:47 +0800

    update example

--- examples/example.py
+++ examples/example.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from cryptocmdsupply import CmcSupplyScraper
 
 for i in range(365):
-    target_date = "01-01-2021"
+    target_date = "01-01-2019"
     target_date = datetime.datetime.strptime(target_date, "%d-%m-%Y")
     target_date += datetime.timedelta(days=i)
     target_date = target_date.strftime("%d-%m-%Y")
@@ -19,7 +19,5 @@ for i in range(365):
     # initialize the scraper without limit and fiat
     scraper = CmcSupplyScraper(target_date=target_date, limit = 3000, fiat = "USD")
     # data to csv
-    scraper.export("csv", name="../../Web3/data/supply/cmc_supply_" + target_date)
-
-
+    scraper.export("csv", name="cmc_supply_" + target_date)
 
